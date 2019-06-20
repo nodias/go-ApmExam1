bf7555b649de5defc4c2d3f3e0f55271f3e229e5
kss <kss@antlinesoft.com>
2019-06-20 09:25:45 +0900

    logger

--- main.go
+++ main.go
@@ -1,41 +1,25 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 
 	"go.elastic.co/apm/module/apmgorilla"
 
 	"github.com/gorilla/mux"
+	common "github.com/nodias/go-ApmCommon"
 	"github.com/nodias/go-ApmExam1/api"
 	"github.com/urfave/negroni"
 )
 
-func logger() *os.File {
-	fpLog, err := os.OpenFile("logfile.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		panic(err)
-	}
-	return fpLog
-}
-
 func main() {
-	//logger
-	fpLog := logger()
-	multiWriter := io.MultiWriter(fpLog, os.Stdout)
-	log.SetOutput(multiWriter)
-	defer fpLog.Close()
-
-	//router
 	router := mux.NewRouter()
 	router.HandleFunc("/users", GetUsers).Methods("GET")
 	router.HandleFunc("/user/{id}", GetUser)
 	router.Use(apmgorilla.Middleware())
-	n := negroni.Classic()
-	n.Use(negroni.NewLogger())
+
+	n := negroni.New(negroni.HandlerFunc(common.LoggingMiddleware))
 	n.UseHandler(router)
 	n.Run(":7001")
 }
